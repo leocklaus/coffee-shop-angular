bb2070bbcf0cdfeda2a0f8c0ff80d65def3f21e9
leocklaus <leocklaus@gmail.com>
2023-07-29 14:15:36 -0300

    feat: project created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LogoSvgComponent } from './navbar/logo-svg/logo-svg.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LogoSvgComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/navbar/logo-svg/logo-svg.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-svg',
+  template: `
+    <svg width="30" height="30" viewBox="0 0 221 242" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M155.951 157.902C155.951 157.902 180.178 91.6594 155.951 91.2028C131.725 90.7463 43.2244 91.2031 18.5036 91.2028C-6.21712 91.2025 24.2849 180.335 31.3584 233H135.186L155.951 197.427C155.951 197.427 233.08 175.194 206.382 132.21" stroke="#00623B" stroke-width="18" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M49.3117 9C44.0756 13.7063 42.1897 19.0497 43.3117 26C44.0916 30.8307 48.7891 32.1347 49.3117 37C49.9662 43.0932 48.0803 47.6508 43.3117 51.5" stroke="#00623B" stroke-width="18" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M89.3117 9C84.0756 13.7063 82.1897 19.0497 83.3117 26C84.0916 30.8307 88.7891 32.1347 89.3117 37C89.9662 43.0932 88.0803 47.6508 83.3117 51.5" stroke="#00623B" stroke-width="18" stroke-linecap="round" stroke-linejoin="round"/>
+    <path d="M129.312 9C124.076 13.7063 122.19 19.0497 123.312 26C124.092 30.8307 128.789 32.1347 129.312 37C129.966 43.0932 128.08 47.6508 123.312 51.5" stroke="#00623B" stroke-width="18" stroke-linecap="round" stroke-linejoin="round"/>
+    </svg>
+  `,
+  styles: [
+  ]
+})
+export class LogoSvgComponent {
+
+}
